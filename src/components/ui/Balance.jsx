0336ad6f61e8953d838f5f659648e8aaf5722c1c
YoannBDL <yoannb64@gmail.com>
2022-02-12 14:24:24 +0100

    Squash this

--- src/components/ui/Balance.jsx
+++ src/components/ui/Balance.jsx
@@ -8,7 +8,7 @@ export const Balance = ({ performance }) => {
 		<Figure>
 			{Object.keys(performance).length <= 0 ? null : (
 				<ResponsiveContainer width="100%" height="100%">
-					<RadarChart cx="50%" cy="50%" outerRadius="65%" startAngle={210} endAngle={570} data={performance.data}>
+					<RadarChart cx="50%" cy="50%" outerRadius="70%" startAngle={210} endAngle={570} data={performance.data}>
 						<PolarGrid radialLines={false} />
 						<PolarRadiusAxis tickCount={6} tick={false} axisLine={false} />
 						<PolarAngleAxis dataKey="kind" tickFormatter={formatTick} tick={{ fill: '#FFFFFF', fontSize: '0.875em' }} />
