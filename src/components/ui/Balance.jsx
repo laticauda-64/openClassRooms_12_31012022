7849d18d0ac5b8e12b1293cac28c446703d3e98a
YoannBDL <yoannb64@gmail.com>
2022-02-12 15:05:40 +0100

    Added translation for performance graph

--- src/components/ui/Balance.jsx
+++ src/components/ui/Balance.jsx
@@ -2,7 +2,16 @@ import { Legend, PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, 
 import styled from 'styled-components';
 
 export const Balance = ({ performance }) => {
-	const formatTick = (id) => performance.kind[id].charAt(0).toUpperCase() + performance.kind[id].slice(1);
+	const frenchTranslation = {
+		cardio: 'Cardio',
+		energy: 'Energie',
+		endurance: 'Endurance',
+		strength: 'Force',
+		speed: 'Vitesse',
+		intensity: 'Intensité',
+	};
+
+	const translate = (id) => frenchTranslation[performance.kind[id]];
 
 	return (
 		<Figure>
@@ -11,7 +20,7 @@ export const Balance = ({ performance }) => {
 					<RadarChart cx="50%" cy="50%" outerRadius="70%" startAngle={210} endAngle={570} data={performance.data}>
 						<PolarGrid radialLines={false} />
 						<PolarRadiusAxis tickCount={6} tick={false} axisLine={false} />
-						<PolarAngleAxis dataKey="kind" tickFormatter={formatTick} tick={{ fill: '#FFFFFF', fontSize: '0.875em' }} />
+						<PolarAngleAxis dataKey="kind" tickFormatter={translate} tick={{ fill: '#FFFFFF', fontSize: '12px' }} />
 						<Radar legendType="none" dataKey="value" stroke="#E60000" fill="#E60000" fillOpacity={0.7} />
 						<Legend />
 					</RadarChart>
