f53e2c6dc4898d0153a79a44ac953dafd3e2e738
YoannBDL <yoannb64@gmail.com>
2022-02-13 14:55:13 +0100

    Better looking 404 component

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,8 +1,27 @@
+import styled from 'styled-components';
+
 export const Error404 = () => {
 	return (
-		<div>
-			<p>Error 404 !</p>
-			<p>Sorry, user or page not found...</p>
-		</div>
+		<ErrorBlock>
+			<div>
+				<p className="bigText">Error 404 !</p>
+				<p>Sorry, user or page not found...</p>
+			</div>
+		</ErrorBlock>
 	);
 };
+
+const ErrorBlock = styled.div`
+	width: 100%;
+	height: 75%;
+	color: red;
+	font-size: 24px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	.bigText {
+		font-size: 84px;
+		font-weight: bold;
+	}
+`;
